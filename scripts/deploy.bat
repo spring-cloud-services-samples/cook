32a47eafc24901a6670e8380cb9c39897f4a623b
Ben Klein <ben@silver-chalice.com>
2015-07-07 14:50:54 -0500

    accept path to archive in deploy.bat

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -1,5 +1,9 @@
 @echo off
-cf create-service p-config-server standard config-server
-echo Please visit Apps Manager and enter a repository URI for the new "config-server" service. Then press any key to continue...
-pause > nul
-cf push
+if "%1"=="" (
+  echo Please provide the path to the application archive.
+) else (
+  cf create-service p-config-server standard config-server
+  echo Please visit Apps Manager and enter a repository URI for the new "config-server" service. Then press any key to continue...
+  pause > nul
+  cf push -p %1
+)
