1c1945548e90fe1cafc996927d154f38df586469
Ben Klein <ben@silver-chalice.com>
2015-09-11 15:22:27 -0500

    remove call to CloudFoundryCertificateTruster

--- src/main/java/cook/Application.java
+++ src/main/java/cook/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import io.pivotal.springcloud.ssl.CloudFoundryCertificateTruster;
 
 @RestController
 @SpringBootApplication
@@ -20,7 +19,6 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        CloudFoundryCertificateTruster.trustApiCertificate();
         SpringApplication.run(Application.class, args);
     }
 
