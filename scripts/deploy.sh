4c423518cafc1de262e02058254fc5f28b87902c
Ben Klein <bklein@pivotal.io>
2016-06-29 14:26:35 -0500

    Update Bash script for Spring Cloud Services 1.1.0

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,15 @@ then
   echo "Please provide the path to the application archive."
   exit 1
 fi
-cf create-service p-config-server standard config-server
-read -p "Please visit Apps Manager and enter a repository URI for the new \"config-server\" service. Then press any key to continue..." -n1 -s
+echo -n "Creating Config Server..."
+{
+  cf create-service -c '{ "git": { "uri": "https://github.com/spring-cloud-samples/cook-config", "label": "master"  } }' p-config-server standard config-server
+} &> /dev/null
+until [ `cf service config-server | grep -c "succeeded"` -eq 1  ]
+do
+  echo -n "."
+done
 echo
+echo "Config Server created. Pushing application."
 cf push -p "$@"
+echo "Done!"
