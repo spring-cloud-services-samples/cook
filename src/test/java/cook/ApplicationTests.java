91c7add0a805249fccf5ea48bdfcc168792e6e88
Chris Sterling <csterling@pivotal.io>
2017-10-18 09:49:42 -0700

    Added generated Gradle directories to ignore. Updated code to use SCS Connectors 1.6.0 snapshot, Spring Cloud Dalston.SR4 and Boot 1.5.8.

--- src/test/java/cook/ApplicationTests.java
+++ src/test/java/cook/ApplicationTests.java
@@ -18,11 +18,11 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringBootTest
 @WebAppConfiguration
 public class ApplicationTests {
 
