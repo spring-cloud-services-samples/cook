1fe01491dc16dc062787850f466332ccd3152931
Ben Klein <bklein@pivotal.io>
2018-03-07 11:11:12 -0600

    Incorporate plain text file

--- src/main/java/cook/Application.java
+++ src/main/java/cook/Application.java
@@ -1,14 +1,22 @@
 package cook;
 
+import java.io.IOException;
+import org.springframework.beans.BeansException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.pivotal.spring.cloud.service.config.PlainTextConfigClient;
+
 @RestController
 @SpringBootApplication
 public class Application {
+
+    @Autowired
+    PlainTextConfigClient configClient;
 
     @Autowired
     private Menu menu;
@@ -23,8 +31,18 @@ public class Application {
       return menu.getSecretMenu();
     }
 
+    @RequestMapping("/restaurant/dessert-menu")
+    String request() throws BeansException, IOException {
+      return convertStreamToString(configClient.getConfigFile("cloud", "master", "dessert.json").getInputStream());
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    static String convertStreamToString(java.io.InputStream is) {
+      java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+      return s.hasNext() ? s.next() : "";
     }
 
 }
