7ea3fa3c5af6a3804ed528b8d6bf35a6d0b40fff
Ben Klein <bklein@pivotal.io>
2018-03-08 09:38:17 -0600

    Revise example of plain text file retrieval

--- src/main/java/cook/Application.java
+++ src/main/java/cook/Application.java
@@ -1,13 +1,15 @@
 package cook;
 
 import java.io.IOException;
-import org.springframework.beans.BeansException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StreamUtils;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 
 import io.pivotal.spring.cloud.service.config.PlainTextConfigClient;
 
@@ -32,17 +34,13 @@ public class Application {
     }
 
     @RequestMapping("/restaurant/dessert-menu")
-    String request() throws BeansException, IOException {
-      return convertStreamToString(configClient.getConfigFile("cloud", "master", "dessert.json").getInputStream());
+    String dessertMenu() throws IOException {
+      InputStream input = configClient.getConfigFile("cloud", "master", "dessert.json").getInputStream();
+      return StreamUtils.copyToString(input, Charset.defaultCharset());
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    static String convertStreamToString(java.io.InputStream is) {
-      java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-      return s.hasNext() ? s.next() : "";
     }
 
 }
