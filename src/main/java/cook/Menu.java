d73742eea978b5ddce328b747a809268c6c222fe
Ben Klein <bklein@pivotal.io>
2018-06-20 17:10:10 -0500

    Remove snippet markers from code

--- src/main/java/cook/Menu.java
+++ src/main/java/cook/Menu.java
@@ -4,23 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
-// code_snippet cookSpecialOne start java
 @RefreshScope
 @Component
 public class Menu {
 
   @Value("${cook.special}")
   String special;
-// code_snippet cookSpecialOne end
 
   @Value("${secretMenu}")
   String secretMenu;
 
-// code_snippet cookSpecialTwo start java
   public String getSpecial() {
     return special;
   }
-// code_snippet cookSpecialTwo end
 
   public String getSecretMenu() {
     return secretMenu;
