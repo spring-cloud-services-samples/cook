c8e57d1653b6f5ec2ff4e75c3b7efd5c10a28584
kvmw <mshamsi@vmware.com>
2022-03-24 10:49:47 +0100

    Bump spring-boot version to 2.4.x

--- src/main/java/cook/DessertMenu.java
+++ src/main/java/cook/DessertMenu.java
@@ -20,16 +20,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 
-import io.pivotal.spring.cloud.service.config.PlainTextConfigClient;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StreamUtils;
+import io.pivotal.spring.cloud.service.config.PlainTextConfigClient;
 
 @Service
 public class DessertMenu {
 
-	private PlainTextConfigClient configClient;
+	private final PlainTextConfigClient configClient;
 
 	public DessertMenu(@Autowired(required = false) PlainTextConfigClient configClient) {
 		this.configClient = configClient;
@@ -39,7 +39,7 @@ public class DessertMenu {
 		if (configClient == null) {
 			return "none";
 		}
-		InputStream input = configClient.getConfigFile("cloud", "master", "dessert.json").getInputStream();
+		InputStream input = configClient.getPlainTextResource("cloud", "master", "dessert.json").getInputStream();
 		return StreamUtils.copyToString(input, Charset.defaultCharset());
 	}
 
