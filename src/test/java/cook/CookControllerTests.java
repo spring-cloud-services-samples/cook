16744d747a21aae1771449848b57c94ca0b9faa8
kvmw <mshamsi@broadcom.com>
2024-12-04 11:49:36 +0100

    Bumps spring boot to v3.4.0 and spring cloud to v2024.0.0
    Signed-off-by: kvmw <mshamsi@broadcom.com>

--- src/test/java/cook/CookControllerTests.java
+++ src/test/java/cook/CookControllerTests.java
@@ -22,8 +22,8 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
 
@@ -38,10 +38,10 @@ public class CookControllerTests {
 	@Autowired
 	private MockMvc mvc;
 
-	@MockBean
+	@MockitoBean
 	private Menu menu;
 
-	@MockBean
+	@MockitoBean
 	private DessertMenu dessertMenu;
 
 	@Test
